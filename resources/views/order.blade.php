389c966ac57bc8d71bc0f5862506d9f10ae1c658
Bohdan <bogdantsvg@gmail.com>
2021-05-26 00:58:48 +0300

    add form

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -4,6 +4,14 @@
 
 
 @section('content')
-    <p>Это содержимое тела страницы.</p>
+    <br><br><br>
+    <form action="order/submit" method="post">
+        Замовлення столиків<br>
+        <input type="text" name="name" placeholder="Ім'я" id="name"><br>
+        <input type="text" name="email" placeholder="Email" id="email"><br>
+        <input type="text" name="comments" placeholder="Кількість людей та час" id="comments"><br>
+        <button type="submit">Замовити</button>
+    </form>
+    <br><br><br>
 @endsection
 
